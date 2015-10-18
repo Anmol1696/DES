e4b496c3a3d3496ad5d6bc7253862dca8e7a4764
Anmol1696 <anmol1696@gmail.com>
2015-10-19 05:18:50 +0530

    Adustment in main.cpp

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main()
 	int command;
 	cout << "1. DES Encryption  " << "2. DES Decryption" << endl;
 	cin >> command;
-	
+
 	conversion ob;
 	string input;
 	string key;
@@ -22,7 +22,7 @@ int main()
 	getline(cin,input);
 	cout << "Enter the key : " << endl;
 	getline(cin,key);
-	
+
 	if(command==1)	ob.text_binary(input);
 	else ob.encrypted_binary(input);
 	ob.IP1();
@@ -30,11 +30,11 @@ int main()
 	ob.PC1();
 	ob.PC2();
 	ob.PC2_con();
-	
+
 	if(command==2) ob.swap_key();
-	
+
 	cout << "\n\n" << endl;
-	
+
 	if(command==1)	 ob.E();
 	else ob.D();
 	ob.IP2();
@@ -44,29 +44,4 @@ int main()
 		ob.print_EncryptedText();
 	}
 	else ob.print_DecryptedText();
-       
-	
-	
-	/*
-	conversion ob;
-	string input;
-	string key;
-	cout << "Enter the value : " << endl;
-	getline(cin,input);
-	cout << "Enter the key : " << endl;
-	getline(cin,key);
-	ob.encrypted_binary(input);
-	ob.IP1();
-	ob.key_binary(key);
-	ob.PC1();	
-	ob.PC2();
-	ob.PC2_con();
-	ob.swap_key();
-	cout << "\n\n" << endl;
-	ob.D();
-	ob.IP2();
-	ob.print_DecryptedText();
-	*/
 }
-
-
